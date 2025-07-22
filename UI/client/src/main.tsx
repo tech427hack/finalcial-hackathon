f5637413e27d1f9a67a315e4f311197f62d7cba8
manohar427 <manohar427@gmail.com>
2025-07-22 10:05:42 +0530

    Hackathon changes

---
+++ UI/client/src/main.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // ✅ Add this
+import App from "./App";
+import "./index.css";
+import "./ChatBot.css";
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter> {/* ✅ Wrap App */}
+    <App />
+  </BrowserRouter>
+);
