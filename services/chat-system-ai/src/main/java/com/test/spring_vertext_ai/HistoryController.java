f2e6300e09b9bcf75b1b87b7599ef315621591d0
manohar427 <manohar427@gmail.com>
2025-07-22 10:53:44 +0530

    Chatbot Service changes

---
+++ services/chat-system-ai/src/main/java/com/test/spring_vertext_ai/HistoryController.java
@@ -0,0 +1,52 @@
+package com.test.spring_vertext_ai;
+
+import java.util.List;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HistoryController {
+
+    private final ChatClient chatClient;
+
+    public HistoryController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping(value = "/user",produces = "application/json")
+	@CrossOrigin(origins = "http://localhost:5000")
+    public String getAnswer(@RequestParam String query) {
+    	System.out.println("Question:"+query);
+    	
+    	Prompt prompt = new Prompt(List.of(new SystemMessage("""
+			    You are an AI Assistant, a polite and knowledgeable AI assistant.
+			    Then answer the user's question politely and clearly.
+			    You are giving the answers for SHD community savings by group of people can form a group
+			    and can do saving out of it Users can take loan with proper details.
+			    From this savings group community can generate profits them self.
+			    Address of SDH community group Sarjapura,Bangalore,India,Ph:9986781724
+			    Admin email address:manohar.s@sdh.com,Name : Manohar Sambayyapalem.
+			    We need to get proper approval to form a community Group.
+			    Once community is formed .. they can conduct meetings and pools in this app.
+			    To join in to community mobile number & Adhar card is mandatory.
+			    Always be respectful and never respond rudely, even if provoked.
+			    Also you can use meaningful icons like 😊 📊 💡 ✍️ based on the situation.
+			"""), new UserMessage(query)));
+    	
+    	ChatResponse cliResponse = chatClient.prompt(prompt)
+				.call()
+				.chatResponse();
+    	String response = cliResponse.getResult().getOutput().getText();
+    	System.out.println("Response:"+response);
+    	return  response;
+    }
+}
---
+++ services/chat-system-ai/src/main/java/com/test/spring_vertext_ai/SpringVertextAiApplication.java
@@ -0,0 +1,12 @@
+package com.test.spring_vertext_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVertextAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVertextAiApplication.class, args);
+	}
+}
